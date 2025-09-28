de42730c98c323dab983ca8e758474c15cc13e4a
japinol7 <japinol@outlook.com>
2025-09-28 18:17:06 +0200

    [IMP] Add some methods to the client api to call services such as: common, db.

--- odoo_jsonrpc/simple_jsonrpc/jsonrpc_client.py
+++ odoo_jsonrpc/simple_jsonrpc/jsonrpc_client.py
@@ -11,39 +11,83 @@ class JsonRpcClient:
         self._server = connection.server
         self._uid = connection.uid
 
+    def get_db_list(self):
+        """Gets the available databases."""
+        return self._connection.call('db', 'list')
+
     def get_server_version(self):
         """Gets the server version."""
         return self._connection.call('common', 'version')['server_version']
 
-    def call(self, model_obj_name, method, values):
-        """Calls a given method on a given model object name
-        with the specified values.
+    def get_server_version_data(self):
+        """Gets the server version data."""
+        return self._connection.call('common', 'version')
+
+    def call_service(self, service_obj_name, method, kwargs=None):
+        """Calls a given method on a given service object name with
+        the specified kwargs. Service name examples: common, db, object.
         """
-        return self._connection.execute(
-            self._server.dbname, self._uid, self._server.password,
-            model_obj_name,
-            method, values,
+        if kwargs is None:
+            return self._connection.call(service_obj_name, method)
+
+        return self._connection.call(
+            service_obj_name,
+            method, kwargs or {},
             )
 
-    def call_kw_on_instances(self, model_obj_name, method, ids, values):
-        """Calls a given method on given model object instances
-        with the specified values.
-        This call uses execute_kw.
+    def call_common(self, method, kwargs=None):
+        """Calls a given method on the common service
+        with the specified kwargs.
         """
+        if kwargs is None:
+            return self._connection.call('common', method)
+
+        return self._connection.call(
+            'common', method, kwargs or {},
+            )
+
+    def call_db(self, method, kwargs=None):
+        """Calls a given method on the db service
+        with the specified kwargs.
+        """
+        if kwargs is None:
+            return self._connection.call('db', method)
+
+        return self._connection.call(
+            'db', method, kwargs or {},
+            )
+
+    def call(self, model_obj_name, method, ids, args=None, kwargs=None):
+        """Calls a given method on given model object instances
+        with the specified positional args and kwargs.
+        Recommended way to call a normal recordset method.
+        """
+        ids = ids or []
+        args = args or []
+        args = [ids] + args
+
         return self._connection.execute_kw(
             self._server.dbname, self._uid, self._server.password,
             model_obj_name,
-            method, ids, values,
+            method, args, kwargs or {},
             )
 
-    def call_on_instances(self, model_obj_name, method, ids, values):
-        """Calls a given method on given model object instances
-        with the specified values.
+    def call_on_model(self, model_obj_name, method, args=None, kwargs=None):
+        """Calls a given method on a given model object name
+        with the specified positional args and kwargs.
+        Recommended way to call an api.model method.
         """
-        return self._connection.execute(
+        if kwargs is None:
+            return self._connection.execute_kw(
+                self._server.dbname, self._uid, self._server.password,
+                model_obj_name,
+                method, args,
+                )
+
+        return self._connection.execute_kw(
             self._server.dbname, self._uid, self._server.password,
             model_obj_name,
-            method, ids, values,
+            method, args, kwargs or {},
             )
 
     def create(self, model_obj_name, values):
@@ -60,6 +104,19 @@ class JsonRpcClient:
             self._server.dbname, self._uid, self._server.password,
             model_obj_name,
             'read', ids, fields,
+            )
+
+    def read_group(
+            self, model_obj_name, domain, fields, group_by, limit=DEFAULT_LIMIT
+        ):
+        """Returns a list of dictionaries with the aggregate results
+        grouped by the `group_by` field.
+        """
+        return self._connection.execute_kw(
+            self._server.dbname, self._uid, self._server.password,
+            model_obj_name,
+            'read_group', domain,
+            {'fields': fields, 'groupby': group_by, 'limit': limit},
             )
 
     def search(self, model_obj_name, domain, order=None, limit=DEFAULT_LIMIT):
